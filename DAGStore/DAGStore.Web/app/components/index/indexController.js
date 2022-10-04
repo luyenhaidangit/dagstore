ad3d9e24ab045c57e4952f627edda107f936f5ee
luyenhaidangit <luyenhaidangit@gmail.com>
2022-10-04 10:44:28 +0700

    Build Feature Render CategoryShowOnHomePage

--- DAGStore/DAGStore.Web/app/components/index/indexController.js
+++ DAGStore/DAGStore.Web/app/components/index/indexController.js
@@ -5,16 +5,16 @@ index.controller('indexController', indexController);
 // Controller
 function indexController($scope, apiService){
     // Get Data
-    $scope.categorysShowOnHomePage = [];
-    $scope.GetCategoryShowOnHomePage = GetCategoryShowOnHomePage;
-    function GetCategoryShowOnHomePage() {
-        apiService.get("/category/getcategoryshowonhomepage", null, function (result) {
+    //$scope.categorysShowOnHomePage = [];
+    //$scope.GetCategoryShowOnHomePage = GetCategoryShowOnHomePage;
+    //function GetCategoryShowOnHomePage() {
+    //    apiService.get("/category/getcategoryshowonhomepage", null, function (result) {
 
-            $scope.categorysShowOnHomePage = result.data;
-            console.log($scope.categorysShowOnHomePage);
-        }, function (error) {
-            console.log("Get data fail");
-        })
-    };
-    $scope.GetCategoryShowOnHomePage();
+    //        $scope.categorysShowOnHomePage = result.data;
+    //        console.log($scope.categorysShowOnHomePage);
+    //    }, function (error) {
+    //        console.log("Get data fail");
+    //    })
+    //};
+    //$scope.GetCategoryShowOnHomePage();
 }
---
+++ DAGStore/DAGStore.Web/app/shared/directives/main-navigation/main-navigationController.js
@@ -0,0 +1,20 @@
+﻿// Register controller
+var app = angular.module("DAGStore.common");
+app.controller("mainNavigationController", mainNavigationController);
+
+// Controller
+function mainNavigationController($scope, apiService) {
+    // Get Data
+    $scope.categorysShowOnHomePage = [];
+    $scope.GetCategoryShowOnHomePage = GetCategoryShowOnHomePage;
+    function GetCategoryShowOnHomePage() {
+        apiService.get("/category/getcategoryshowonhomepage", null, function (result) {
+
+            $scope.categorysShowOnHomePage = result.data;
+            console.log($scope.categorysShowOnHomePage);
+        }, function (error) {
+            console.log("Get data fail");
+        })
+    };
+    $scope.GetCategoryShowOnHomePage();
+};
