509955ee696f6039525024f4593fde2d59175935
luyenhaidangit <luyenhaidangit@gmail.com>
2022-10-08 06:37:01 +0700

    Build Feature Render Product Detailt - Home Page

--- DAGStore/DAGStore.Web/app/components/client/product/productController.js
+++ DAGStore/DAGStore.Web/app/components/client/product/productController.js
@@ -1,8 +1,31 @@
-// Register controller
+﻿// Register controller
 var product = angular.module('DAGStoreHome.product');
 product.controller('productController', productController);
 
 // Controller
 function productController($scope, apiService, $stateParams, $filter) {
+    // Load Product Detail
+    $scope.product = {
+    }
+    $scope.category = {
+    }
+
+    apiService.get("/product/getbyid/" + $stateParams.id, null, function (result) {
+        $scope.product = result.data;
+        $scope.product.CreateOn = $filter('formatJsonDate')($scope.product.CreateOn);
+        apiService.get("/category/getbyid/" + $scope.product.CategoryID, null, function (resultCategory) {
+            $scope.category = resultCategory.data;
+            console.log($scope.category);
+        }, function (error) {
+            console.log("Không thể tải dữ liệu");
+            
+        })
+    }, function (error) {
+        console.log("Không thể tải dữ liệu");
+    })
+
+   
+    
+
     
 }
--- DAGStore/DAGStore.Web/app/shared/directives/main-navigation/main-navigationDirective.js
+++ DAGStore/DAGStore.Web/app/shared/directives/main-navigation/main-navigationDirective.js
@@ -1,6 +1,5 @@
 ﻿var app = angular.module("DAGStore.common");
 app.directive("mainNavigationDirective", function () {
-    console.log("okdsfajkldfj")
     return {
         restrict: "EA",
         templateUrl: '/app/shared/directives/main-navigation/main-navigationView.html',
