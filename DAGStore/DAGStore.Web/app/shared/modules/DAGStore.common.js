b2ae0f48f02b6f2c4a854cded7a0f8042054e015
luyenhaidangit <luyenhaidangit@gmail.com>
2022-11-19 00:52:25 +0700

    Style Top Loading Bar Admin

--- DAGStore/DAGStore.Web/app/shared/modules/DAGStore.common.js
+++ DAGStore/DAGStore.Web/app/shared/modules/DAGStore.common.js
@@ -1,2 +1,8 @@
 // Register module
-var common = angular.module('DAGStore.common', ['ui.router', 'oc.lazyLoad','angular-loading-bar'])
+var common = angular.module('DAGStore.common', ['ui.router', 'oc.lazyLoad', 'angular-loading-bar'])
+
+common.config(['cfpLoadingBarProvider', function (cfpLoadingBarProvider) {
+    cfpLoadingBarProvider.includeSpinner = false;
+    cfpLoadingBarProvider.parentSelector = '#loading-bar-container';
+    cfpLoadingBarProvider.spinnerTemplate = '<div><span class="fa fa-spinner">Custom Loading Message...</div>';
+    }])
