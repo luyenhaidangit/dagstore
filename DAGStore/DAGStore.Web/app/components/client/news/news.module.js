32e2dcfaddd51ac74e10712492dd929c52a9716c
luyenhaidangit <luyenhaidangit@gmail.com>
2022-12-22 13:44:31 +0700

    Build Template List News

--- DAGStore/DAGStore.Web/app/components/client/news/news.module.js
+++ DAGStore/DAGStore.Web/app/components/client/news/news.module.js
@@ -6,6 +6,15 @@ news.config(function ($stateProvider, $urlRouterProvider) {
     // Config Router
     var states = [
         {
+            name: 'listNews',
+            url: '/listnews',
+            templateUrl: '/app/components/client/news/listNewsView.html',
+            controller: "listNewsController",
+            onEnter: function () {
+                $('html, body').animate({ scrollTop: -10000 }, 0);
+            }
+        },
+        {
             name: 'news',
             url: '/news/:id',
             templateUrl: '/app/components/client/news/newsView.html',
